d46cfb232ad00f0ff7cf1636cc9f3d37bd3254e6
Asher Willner <awillner2016@yula.org>
2019-07-14 13:17:30 -0700

    Initial Commit

---
+++ CryptoCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoCalc
+//
+//  Created by Asher Willner on 7/14/19.
+//  Copyright © 2019 Asher Willner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
