6fd2fcc0f4088174ceae5816e82027c2e44cb459
Asher Willner <awillner2016@yula.org>
2019-07-15 22:56:37 -0700

    Grabbing close data of inputted crypto since inception date and calculating daily returns and daily standard deviation

--- CryptoCalc/ViewController.swift
+++ CryptoCalc/ViewController.swift
@@ -9,13 +9,12 @@
 import UIKit
 import SwiftyJSON
 import Alamofire
+import Accelerate
 
 class ViewController: UIViewController {
 
     var ticker : String = ""
-    
-    
-    
+    var closeArray = [Double]()
     
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var tickerPicker: UITextField!
@@ -27,8 +26,7 @@ class ViewController: UIViewController {
 
     @IBAction func getTickerPressed(_ sender: Any) {
         ticker = tickerPicker.text!.uppercased()
-        let URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/\(ticker)USD"
-        print(URL)
+        let URL = "https://min-api.cryptocompare.com/data/histoday?fsym=\(ticker)&tsym=USD&allData=true"
         getBitcoinData(url: URL)
     }
     
@@ -40,7 +38,28 @@ class ViewController: UIViewController {
 
                 print("Sucess! Got the bitcoin data")
                 let bitcoinJSON : JSON = JSON(response.result.value!)
-
+                let data = bitcoinJSON["Data"]
+                var count = data.count - 1
+                while count >= 0 {
+                    let closeValue = data[count]["close"].double!
+                    self.closeArray.append(closeValue)
+                    count -= 1
+                }
+               var returns = [Double]()
+                for i in 0...(self.closeArray.count-2) {
+                    let placeholder = log(self.closeArray[i+1]/self.closeArray[i])
+                    returns.append(placeholder)
+                }
+                
+//                print(returns)
+                
+                var mn = 0.0
+                var sddev = 0.0
+                vDSP_normalizeD(returns, 1, nil, 1, &mn, &sddev, vDSP_Length(returns.count))
+                sddev *= sqrt(Double(returns.count)/Double(returns.count - 1))
+                
+                print(mn, round(sddev*1000)/1000)
+                
                 self.updateBitcoinData(json: bitcoinJSON)
 
             } else {
@@ -53,8 +72,9 @@ class ViewController: UIViewController {
 
     func updateBitcoinData(json : JSON) {
 
-        if var priceResult = json["last"].double {
+        if var priceResult = json["Data"][0]["close"].double {
             priceResult = round(priceResult*100) / 100
+           
             priceLabel.text = "$" + String(priceResult)
         } else {
             priceLabel.text = "Connection Issues"
