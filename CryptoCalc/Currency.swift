66d8f60f3e50f645a81e3cd73be5f563b085ee6e
Asher Willner <awillner2016@yula.org>
2019-07-16 22:30:59 -0700

    Creating dictionary of accessed crypto symbols which contains ticker and standard deviation of each crypto. Also added image assets and app icon.

---
+++ CryptoCalc/Currency.swift
@@ -0,0 +1,16 @@
+//
+//  Currency.swift
+//  CryptoCalc
+//
+//  Created by Asher Willner on 7/16/19.
+//  Copyright © 2019 Asher Willner. All rights reserved.
+//
+
+import Foundation
+
+class Currency {
+    var symbol : String = ""
+    var std : Double = 0
+    var risk : String = ""
+    var age : Int = 0
+}
--- CryptoCalc/ViewController.swift
+++ CryptoCalc/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 import SwiftyJSON
 import Alamofire
 import Accelerate
+import SVProgressHUD
 
 class ViewController: UIViewController {
 
     var ticker : String = ""
-    var closeArray = [Double]()
+    var stdDict = [String:Double]()
+    var riskDict = [String:String]()
+//    var currencyObjects = [String:Currency]()
     
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var tickerPicker: UITextField!
@@ -25,9 +28,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func getTickerPressed(_ sender: Any) {
+        SVProgressHUD.show()
         ticker = tickerPicker.text!.uppercased()
         let URL = "https://min-api.cryptocompare.com/data/histoday?fsym=\(ticker)&tsym=USD&allData=true"
         getBitcoinData(url: URL)
+        view.endEditing(true)
     }
     
     func getBitcoinData(url: String) {
@@ -35,51 +40,63 @@ class ViewController: UIViewController {
         Alamofire.request(url, method: .get).responseJSON {
             response in
             if response.result.isSuccess {
-
+                var closeArray = [Double]()
                 print("Sucess! Got the bitcoin data")
                 let bitcoinJSON : JSON = JSON(response.result.value!)
                 let data = bitcoinJSON["Data"]
                 var count = data.count - 1
                 while count >= 0 {
                     let closeValue = data[count]["close"].double!
-                    self.closeArray.append(closeValue)
+                    closeArray.append(closeValue)
                     count -= 1
                 }
                var returns = [Double]()
-                for i in 0...(self.closeArray.count-2) {
-                    let placeholder = log(self.closeArray[i+1]/self.closeArray[i])
+                for i in 0...(closeArray.count-2) {
+                    let placeholder = log(closeArray[i+1]/closeArray[i])
                     returns.append(placeholder)
                 }
-                
-//                print(returns)
                 
                 var mn = 0.0
                 var sddev = 0.0
                 vDSP_normalizeD(returns, 1, nil, 1, &mn, &sddev, vDSP_Length(returns.count))
                 sddev *= sqrt(Double(returns.count)/Double(returns.count - 1))
                 
-                print(mn, round(sddev*1000)/1000)
+                let std = round(sddev*1000)/1000
                 
-                self.updateBitcoinData(json: bitcoinJSON)
-
+                print(mn, std)
+                SVProgressHUD.dismiss()
+                self.priceLabel.text = String(round(sddev*1000)/1000)
+                
+                self.stdDict[self.ticker] = std
+                
+//                print(self.stdDict)
+                let dictValInc = self.stdDict.sorted(by: { $0.value < $1.value })
+//                print(dictValInc)
+                
+//                let currencyObj = Currency()
+//
+                if std < 0.1 {
+                    self.riskDict[self.ticker] = "Fair Risk"
+                } else if std < 0.2 {
+                    self.riskDict[self.ticker] = "High Risk"
+                } else {
+                    self.riskDict[self.ticker] = "Avoid"
+                }
+                print(self.riskDict)
+                
+//                currencyObj.symbol = self.ticker
+//                currencyObj.std = std
+//                currencyObj.age = data.count
+//
+//                self.currencyObjects[self.ticker] = currencyObj
+//
+//                print(self.currencyObjects[self.ticker]?.risk)
+                
             } else {
                 print("Error: \(String(describing: response.result.error))")
                 self.priceLabel.text = "Connection Issues"
             }
         }
-
-    }
-
-    func updateBitcoinData(json : JSON) {
-
-        if var priceResult = json["Data"][0]["close"].double {
-            priceResult = round(priceResult*100) / 100
-           
-            priceLabel.text = "$" + String(priceResult)
-        } else {
-            priceLabel.text = "Connection Issues"
-        }
-
 
     }
 
