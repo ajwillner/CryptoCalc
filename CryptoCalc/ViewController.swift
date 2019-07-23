08c2e15273bc315d91e8fc92e41c5bc0097a262c
Asher Willner <awillner2016@yula.org>
2019-07-22 17:40:49 -0700

    Converted model to using a data model instead of a dictionary of [Crypto:Risk]

--- CryptoCalc/ViewController.swift
+++ CryptoCalc/ViewController.swift
@@ -11,13 +11,12 @@ import SwiftyJSON
 import Alamofire
 import Accelerate
 import SVProgressHUD
+import SigmaSwiftStatistics
 
 class ViewController: UIViewController {
 
-    var ticker : String = ""
-    var stdDict = [String:Double]()
-    var riskDict = [String:String]()
-//    var currencyObjects = [String:Currency]()
+    var currencyObjects = [Currency]()
+    var counter = 0
     
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var tickerPicker: UITextField!
@@ -25,25 +24,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadCurrencies()
     }
 
     @IBAction func getTickerPressed(_ sender: Any) {
-        SVProgressHUD.show()
-        ticker = tickerPicker.text!.uppercased()
-        let URL = "https://min-api.cryptocompare.com/data/histoday?fsym=\(ticker)&tsym=USD&allData=true"
-        getBitcoinData(url: URL)
+
+        priceLabel.text = currencyObjects[19].risk
         view.endEditing(true)
     }
     
-    func getBitcoinData(url: String) {
-
+    func getBitcoinData(ticker: String, placeholder: Int) {
+        let url = "https://min-api.cryptocompare.com/data/histoday?fsym=\(ticker)&tsym=USD&allData=true"
         Alamofire.request(url, method: .get).responseJSON {
             response in
             if response.result.isSuccess {
                 var closeArray = [Double]()
-                print("Sucess! Got the bitcoin data")
                 let bitcoinJSON : JSON = JSON(response.result.value!)
                 let data = bitcoinJSON["Data"]
+                self.currencyObjects[placeholder].age = data.count
                 var count = data.count - 1
                 while count >= 0 {
                     let closeValue = data[count]["close"].double!
@@ -62,35 +60,23 @@ class ViewController: UIViewController {
                 sddev *= sqrt(Double(returns.count)/Double(returns.count - 1))
                 
                 let std = round(sddev*1000)/1000
-                
-                print(mn, std)
-                SVProgressHUD.dismiss()
-                self.priceLabel.text = String(round(sddev*1000)/1000)
-                
-                self.stdDict[self.ticker] = std
-                
-//                print(self.stdDict)
-                let dictValInc = self.stdDict.sorted(by: { $0.value < $1.value })
-//                print(dictValInc)
-                
-//                let currencyObj = Currency()
+//                let standard = Sigma.standardDeviationSample(returns)
+                self.currencyObjects[placeholder].std = std
+//                print(std)
+//                SVProgressHUD.dismiss()
+//                self.priceLabel.text = String(round(sddev*1000)/1000)
 //
                 if std < 0.1 {
-                    self.riskDict[self.ticker] = "Fair Risk"
+                    self.currencyObjects[placeholder].risk = "Fair Risk"
                 } else if std < 0.2 {
-                    self.riskDict[self.ticker] = "High Risk"
+                    self.currencyObjects[placeholder].risk = "High Risk"
                 } else {
-                    self.riskDict[self.ticker] = "Avoid"
+                    self.currencyObjects[placeholder].risk = "Avoid"
                 }
-                print(self.riskDict)
                 
-//                currencyObj.symbol = self.ticker
-//                currencyObj.std = std
-//                currencyObj.age = data.count
-//
-//                self.currencyObjects[self.ticker] = currencyObj
-//
-//                print(self.currencyObjects[self.ticker]?.risk)
+//                print(self.currencyObjects[placeholder].symbol)
+//                print(self.currencyObjects[placeholder].std)
+//                print(self.currencyObjects[placeholder].risk)
                 
             } else {
                 print("Error: \(String(describing: response.result.error))")
@@ -98,6 +84,131 @@ class ViewController: UIViewController {
             }
         }
 
+    }
+    
+    func loadCurrencies() {
+        
+        let BTC = Currency()
+        currencyObjects.append(BTC)
+        BTC.symbol = "BTC"
+        getBitcoinData(ticker: BTC.symbol, placeholder: counter)
+        counter+=1
+        
+        let ETH = Currency()
+        currencyObjects.append(ETH)
+        ETH.symbol = "ETH"
+        getBitcoinData(ticker: ETH.symbol, placeholder: counter)
+        counter+=1
+        
+        
+        let EOS = Currency()
+        currencyObjects.append(EOS)
+        EOS.symbol = "EOS"
+        getBitcoinData(ticker: EOS.symbol, placeholder: counter)
+        counter+=1
+        
+        
+        let LTC = Currency()
+        currencyObjects.append(LTC)
+        LTC.symbol = "LTC"
+        getBitcoinData(ticker: LTC.symbol, placeholder: counter)
+        counter+=1
+        
+        let XRP = Currency()
+        currencyObjects.append(XRP)
+        XRP.symbol = "XRP"
+        getBitcoinData(ticker: XRP.symbol, placeholder: counter)
+        counter+=1
+        
+        let BCH = Currency()
+        currencyObjects.append(BCH)
+        BCH.symbol = "BCH"
+        getBitcoinData(ticker: BCH.symbol, placeholder: counter)
+        counter+=1
+        
+        let BGG = Currency()
+        currencyObjects.append(BGG)
+        BGG.symbol = "BGG"
+        getBitcoinData(ticker: BGG.symbol, placeholder: counter)
+        counter+=1
+        
+        let BNB = Currency()
+        currencyObjects.append(BNB)
+        BNB.symbol = "BNB"
+        getBitcoinData(ticker: BNB.symbol, placeholder: counter)
+        counter+=1
+        
+        let TRX = Currency()
+        currencyObjects.append(TRX)
+        TRX.symbol = "TRX"
+        getBitcoinData(ticker: TRX.symbol, placeholder: counter)
+        counter+=1
+        
+        let ETC = Currency()
+        currencyObjects.append(ETC)
+        ETC.symbol = "ETC"
+        getBitcoinData(ticker: ETC.symbol, placeholder: counter)
+        counter+=1
+        
+        let ZEC = Currency()
+        currencyObjects.append(ZEC)
+        ZEC.symbol = "ZEC"
+        getBitcoinData(ticker: ZEC.symbol, placeholder: counter)
+        counter+=1
+        
+        let BSV = Currency()
+        currencyObjects.append(BSV)
+        BSV.symbol = "BSV"
+        getBitcoinData(ticker: BSV.symbol, placeholder: counter)
+        counter+=1
+        
+        let NEO = Currency()
+        currencyObjects.append(NEO)
+        NEO.symbol = "NEO"
+        getBitcoinData(ticker: NEO.symbol, placeholder: counter)
+        counter+=1
+        
+        let QTUM = Currency()
+        currencyObjects.append(QTUM)
+        QTUM.symbol = "QTUM"
+        getBitcoinData(ticker: QTUM.symbol, placeholder: counter)
+        counter+=1
+        
+        let USDT = Currency()
+        currencyObjects.append(USDT)
+        USDT.symbol = "USDT"
+        getBitcoinData(ticker: USDT.symbol, placeholder: counter)
+        counter+=1
+        
+        let ZB = Currency()
+        currencyObjects.append(ZB)
+        ZB.symbol = "ZB"
+        getBitcoinData(ticker: ZB.symbol, placeholder: counter)
+        counter+=1
+        
+        let ONT = Currency()
+        currencyObjects.append(ONT)
+        ONT.symbol = "ONT"
+        getBitcoinData(ticker: ONT.symbol, placeholder: counter)
+        counter+=1
+        
+        let DASH = Currency()
+        currencyObjects.append(DASH)
+        DASH.symbol = "DASH"
+        getBitcoinData(ticker: DASH.symbol, placeholder: counter)
+        counter+=1
+        
+        let HT = Currency()
+        currencyObjects.append(HT)
+        HT.symbol = "HT"
+        getBitcoinData(ticker: HT.symbol, placeholder: counter)
+        counter+=1
+        
+        let XLM = Currency()
+        currencyObjects.append(XLM)
+        XLM.symbol = "XLM"
+        getBitcoinData(ticker: XLM.symbol, placeholder: counter)
+        counter+=1
     }
 
 }
